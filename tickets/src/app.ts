6eb38851d02edbc532f542587af14e48316899ed
Rushikesh <rushikeshkhillari10@gmail.com>
2025-01-25 01:56:03 +0530

    project setup

---
+++ tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@curator-ticketing/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.all("*", async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+  return;
+});
+
+export { app };
