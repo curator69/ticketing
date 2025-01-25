846a44008896e706d34a2b7a9de9673d0d1043b2
Rushikesh <rushikeshkhillari10@gmail.com>
2025-01-25 16:08:17 +0530

    adding auth protection

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,12 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError } from "@curator-ticketing/common";
+import {
+  currentUser,
+  errorHandler,
+  NotFoundError,
+} from "@curator-ticketing/common";
+
 import { createTicketRouter } from "./routes/new";
 
 const app = express();
@@ -12,6 +17,8 @@ app.use(json());
 app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
 );
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
