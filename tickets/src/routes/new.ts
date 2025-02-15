fa202540d4bd1cbd4cd8cf1ccc6330f69072c12b
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-15 22:15:28 +0530

    accessing the nats client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { Ticket } from "../models/Ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -25,7 +26,7 @@ router.post(
     });
     await ticket.save();
 
-    await new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
