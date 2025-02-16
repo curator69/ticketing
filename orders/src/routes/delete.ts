d4c887865679c4acd4628b2dc1d80c8beb3fcf13
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-17 00:46:32 +0530

    publishing order cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   NotAuthorizedError,
 } from "@curator-ticketing/common";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -12,7 +14,7 @@ router.delete(
   "/api/orders/:orderId",
   requireAuth,
   async (req: Request, res: Response) => {
-    const order = await Order.findById(req.params.orderId);
+    const order = await Order.findById(req.params.orderId).populate("ticket");
 
     if (!order) {
       throw new NotFoundError();
@@ -24,6 +26,14 @@ router.delete(
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
