e966b835e0d0ea3889f1aad6a1ad5f442988732e
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-16 21:50:02 +0530

    order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@curator-ticketing/common";
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@curator-ticketing/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -17,6 +23,21 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    // Find the ticket the user is trying to order in the database
+    const { ticketId } = req.body;
+
+    // Make sure that this ticket is not already reserved
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
