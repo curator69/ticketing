09964ccfff30e140a9b300a66de78253decfe5d0
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-20 00:10:14 +0530

    testing the ticket updated listener

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,41 @@
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+import mongoose from "mongoose";
+import { TicketUpdatedEvent } from "@curator-ticketing/common";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  ticket.save();
+
+  // create a fake data event
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new concert",
+    price: 999,
+    userId: "asdf",
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  // return all of this stuff
+  return { listener, data, msg, ticket };
+};
+
+it("finds, updates, and saves a ticket", async () => {});
+
+it("acks the message", async () => {});
