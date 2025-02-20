7ac09e5d952d67eb39183bc34f33f0cb6f046a53
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-21 02:11:51 +0530

    rejecting edits of reserved tickets

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 
 import { Ticket } from "../models/Ticket";
 import {
+  BadRequestError,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -28,6 +29,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
