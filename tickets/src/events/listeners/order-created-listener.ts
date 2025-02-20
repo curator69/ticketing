a28dde0cd40ad2285b852124aacb2e4b0df36799
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-21 00:50:08 +0530

    reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,28 @@ import {
 } from "@curator-ticketing/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/Ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
+  }
 }
