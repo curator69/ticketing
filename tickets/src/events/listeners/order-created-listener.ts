a0e92f00801b625ea01c8e27e19a89b953de70ed
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-21 01:27:57 +0530

    publishing while listening

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -6,6 +6,7 @@ import {
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/Ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated.publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -25,6 +26,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // save the ticket
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
 
     // ack the message
     msg.ack();
