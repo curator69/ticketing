1c1175f3625f6f30f47014a534c6c9c2c080d40d
Rushikesh <rushikeshkhillari10@gmail.com>
2025-02-21 23:10:05 +0530

    listener creation

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,14 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from "@curator-ticketing/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
